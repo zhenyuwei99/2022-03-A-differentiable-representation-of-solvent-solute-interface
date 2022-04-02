8728c906256823c125d6f6aed6ed3f257faa3fd0
Zhenyu Wei <zhenyuwei99@gmail.com>
2022-04-03 00:46:39 +0800

    Update visualize result

--- code/run/out/image/3SOS_5/vis.tcl
+++ code/run/out/image/3SOS_5/vis.tcl
@@ -1,12 +1,12 @@
 
-mol new 3RM6_22.pdb
+mol new 3SOS_5.pdb
 mol delrep 0 top
 mol selection \{protein\}
 mol representation CPK 1.4 0.5 8 8
 mol addrep top
 
-mol new 3RM6_22.xyz
+mol new 3SOS_5.xyz
 mol delrep 0 top
-mol representation QuickSurf
+mol representation Line
 mol addrep top
 
