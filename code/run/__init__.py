f429626cd2abe5963b6a87d7e012af3f9f97c219
Zhenyu Wei <zhenyuwei99@gmail.com>
2022-04-03 16:05:31 +0800

    Update run package, add device and data_type vairable, add network_info and traing_info vairbale

--- code/run/__init__.py
+++ code/run/__init__.py
@@ -24,7 +24,7 @@ num_heads = 4
 # Traning Hyperparameters
 is_training_restart = not True
 batch_size = 1
-max_num_samples = 750
+max_num_samples = 250
 num_epochs = 500
 num_proteins_per_epoch = 5000
 learning_rate = 1e-5
@@ -32,6 +32,35 @@ dropout_prob = 0.1
 weight_decay = 0
 save_interval = 100
 log_interval = 50
+# Other hyperparameters
+device = torch.device('cuda')
+data_type = torch.float32
+
+# Network info
+network_info = '''# Network hyperparmeters
+```python
+dim_model = %d
+dim_ffn = %d
+dim_k = dim_v = %d
+num_layers = %d
+num_heads = %d
+```
+''' %(dim_model, dim_ffn, dim_k, num_layers, num_heads)
+# Traning info
+training_info =  '''
+```python
+batch_size = %d
+max_num_samples = %d
+num_epochs = %d
+num_proteins_per_epoch = %d
+learning_rate = %1e
+dropout_prob = %.2f
+weight_decay = %.2f
+```
+''' %(
+    batch_size, max_num_samples, num_epochs, num_proteins_per_epoch,
+    learning_rate, dropout_prob, weight_decay
+)
 
 # Other dirs
 cur_dir = os.path.dirname(os.path.abspath(__file__))
@@ -40,7 +69,7 @@ out_dir = os.path.join(cur_dir, './out/model/')
 directory_file = os.path.join(out_dir, 'directory.txt')
 train_dataset_file = os.path.join(dataset_dir, 'train.h5')
 test_dataset_file = os.path.join(dataset_dir, 'test.h5')
-log_file = os.path.join(out_dir, 'train.log')
+log_file = os.path.join(out_dir, 'train_log.md')
 model_file = os.path.join(out_dir, 'model.pt')
 # Parse directory
 directory, directory_size = parse_directory(directory_file)
@@ -51,7 +80,8 @@ def init_model(max_sequence_length: int):
         dim_model=dim_model, dim_k=dim_k, dim_ffn=dim_ffn,
         directory_size=directory_size,
         num_layers=num_layers, num_heads=num_heads,
-        dropout=dropout_prob, max_sequence_length=max_sequence_length + 100
+        dropout=dropout_prob, max_sequence_length=max_sequence_length + 100,
+        data_type=data_type, device=device
     )
     return model
 
